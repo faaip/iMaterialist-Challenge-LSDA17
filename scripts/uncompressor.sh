92a162b6645ea013a6b29032e1a9581b0e50aa27
Frederik Tollund Juutilainen <frederik.juutilainen@alexandra.dk>
2018-05-13 15:17:11 +0200

    better uncompress script

--- scripts/uncompressor.sh
+++ scripts/uncompressor.sh
@@ -8,7 +8,7 @@ FILES=data_uncompressed/train_chunk*
 for f in $FILES
 do
   echo "Unzipping $f file..."
-  unzip $f -d data/train
+  tar -vx -k -f $f -C data/train
 done
 
 unzip test.zip -d data/test
