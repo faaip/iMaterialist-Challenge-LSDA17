2d3734a8045603e62af6484c27c65ebe955b8af1
Frederik Tollund Juutilainen <frederik.juutilainen@alexandra.dk>
2018-05-13 14:52:33 +0200

    added uncompressor script

---
+++ scripts/uncompressor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir data
+mkdir data/train
+mkdir data/test
+mkdir data/valid
+
+FILES=data_uncompressed/train_chunk*
+for f in $FILES
+do
+  echo "Unzipping $f file..."
+  unzip $f -d data/train
+done
+
+unzip test.zip -d data/test
+unzip valid.zip -d data/valid
