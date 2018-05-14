2388378f1fc615c288853cabde2dd4c0d6380ac0
Frederik Tollund Juutilainen <frederik.juutilainen@alexandra.dk>
2018-05-14 07:02:16 +0200

    updated script

--- as_script.py
+++ as_script.py
@@ -1,76 +1,140 @@
-import numpy as np  
-from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img  
-from keras.models import Sequential  
-from keras.layers import Dropout, Flatten, Dense  
-from keras import applications  
-from keras.utils.np_utils import to_categorical  
-import matplotlib.pyplot as plt  
-import math  
-import cv2  
+import math
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from keras import applications
+from keras.layers import Dense, Dropout, Flatten
+from keras.models import Sequential
+from keras.preprocessing.image import (ImageDataGenerator, img_to_array,
+                                       load_img)
+from keras.utils.np_utils import to_categorical
 
 img_width, img_height = 224, 224  # image dimensions
 
 top_model_weights_path = 'models/bottleneck_fc_model.h5'  # the top layer
 train_data_dir = '../data/train/'
+train_data_dir = 'toy_data/train/'
 validation_data_dir = '../data/valid/'
+validation_data_dir = 'toy_data/valid/'
 
 epochs = 50
-batch_size = 128
+batch_size = 32
 
-# RELOADING
+model = applications.VGG16(include_top=False, weights='imagenet')
+
+datagen = ImageDataGenerator(rescale=1. / 255)
+
+generator = datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode=None,
+    shuffle=False)
+
+nb_train_samples = len(generator.filenames)
+num_classes = len(generator.class_indices)
+
+predict_size_train = int(math.ceil(nb_train_samples / batch_size))
+
+bottleneck_features_train = model.predict_generator(
+    generator, predict_size_train, verbose=1)
+
+np.save('bottleneck_features_train.npy', bottleneck_features_train)
+
+generator = datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode=None,
+    shuffle=False)
+
+nb_validation_samples = len(generator.filenames)
+
+predict_size_validation = int(math.ceil(nb_validation_samples / batch_size))
+
+bottleneck_features_validation = model.predict_generator(
+    generator, predict_size_validation, verbose=1)
+
+np.save('bottleneck_features_validation.npy', bottleneck_features_validation)
+
 # labels for training data
-datagen_top = ImageDataGenerator(rescale=1./255)  
-generator_top = datagen_top.flow_from_directory(  
-             train_data_dir,  
-                  target_size=(img_width, img_height),  
-                       batch_size=batch_size,  
-                            class_mode='categorical',  
-                                 shuffle=False)  
+datagen_top = ImageDataGenerator(rescale=1. / 255)
+generator_top = datagen_top.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical',
+    shuffle=False)
 
-nb_train_samples = len(generator_top.filenames)  
-num_classes = len(generator_top.class_indices)  
+nb_train_samples = len(generator_top.filenames)
+num_classes = len(generator_top.class_indices)
 
-# load the bottleneck features saved earlier  
-train_data = np.load('bottleneck_features_train.npy')  
+# load the bottleneck features saved earlier
+train_data = np.load('bottleneck_features_train.npy')
 
-# get the class lebels for the training data, in the original order  
-train_labels = generator_top.classes  
+# get the class lebels for the training data, in the original order
+train_labels = generator_top.classes
 
-# convert the training labels to categorical vectors  
-train_labels = to_categorical(train_labels, num_classes=num_classes) 
+# convert the training labels to categorical vectors
+train_labels = to_categorical(train_labels, num_classes=num_classes)
 
 # labels for validation features
-generator_top = datagen_top.flow_from_directory(  
-             validation_data_dir,  
-                  target_size=(img_width, img_height),  
-                       batch_size=batch_size,  
-                            class_mode=None,  
-                                 shuffle=False)  
+generator_top = datagen_top.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode=None,
+    shuffle=False)
 
-nb_validation_samples = len(generator_top.filenames)  
+nb_validation_samples = len(generator_top.filenames)
 
-validation_data = np.load('bottleneck_features_validation.npy')  
-validation_labels = generator_top.classes  
-validation_labels = to_categorical(validation_labels, num_classes=num_classes) 
+validation_data = np.load('bottleneck_features_validation.npy')
 
+validation_labels = generator_top.classes
+validation_labels = to_categorical(validation_labels, num_classes=num_classes)
+
+# TRAIN TOP MODEL
 model = Sequential()  
 model.add(Flatten(input_shape=train_data.shape[1:]))  
 model.add(Dense(256, activation='relu'))  
 model.add(Dropout(0.5))  
 model.add(Dense(num_classes, activation='sigmoid'))  
 
-model.compile(optimizer='rmsprop',  
-                  loss='categorical_crossentropy', metrics=['accuracy'])  
+model.compile(optimizer='adam',  
+          loss='categorical_crossentropy', metrics=['accuracy'])  
 
 history = model.fit(train_data, train_labels,  
-              epochs=epochs,  
-                    batch_size=batch_size,  
-                          validation_data=(validation_data, validation_labels))  
+      epochs=epochs,  
+      batch_size=batch_size,  
+      validation_data=(validation_data, validation_labels))  
 
 model.save_weights(top_model_weights_path)  
 
 (eval_loss, eval_accuracy) = model.evaluate(  
-         validation_data, validation_labels, batch_size=batch_size, verbose=1)
+ validation_data, validation_labels, batch_size=batch_size, verbose=1)
 
 print("[INFO] accuracy: {:.2f}%".format(eval_accuracy * 100))  
 print("[INFO] Loss: {}".format(eval_loss)) 
+
+# plot 
+plt.figure(1)
+
+# summarize history for accuracy
+plt.subplot(211)
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+
+# summarize history for loss
+plt.subplot(212)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.savefig('batch_size_'+ str(batch_size) + '.png')
